94d221ac353fb997462fc40536a16e8b1d8fc073
Abao Zhang <abaodoit@gmail.com>
2024-03-28 18:40:02 +0800

    misc: update neovim api

--- lua/integrator/health.lua
+++ lua/integrator/health.lua
@@ -2,13 +2,13 @@ local M = {}
 local integrator = require("integrator")
 
 function M.check()
-    vim.health.report_start("integrator.nvim")
+    vim.health.start("integrator.nvim")
     local modules = { "dap", "session", "settings" }
     for _, module_name in ipairs(modules) do
         if integrator.config[module_name].enabled then
-            vim.health.report_ok(string.format("%s enabled", module_name))
+            vim.health.ok(string.format("%s enabled", module_name))
         else
-            vim.health.report_warn(string.format("%s disabled", module_name))
+            vim.health.warn(string.format("%s disabled", module_name))
         end
     end
 end
